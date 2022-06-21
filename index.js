421b748bf7a0199038aa41bd4d364617abd7cda0
CristianMP04 <christian.marti.pernin@gmail.com>
2022-06-21 16:29:10 +0200

    actualizacion inte 2
    actualizacion inte 2

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 class SortedList {
   constructor() {
     this.items = [];
-    this.length = 0; 
+    this.length = this.items.length; 
   }
 
-  add(item) {}
-  //  this.items.push(item);
-  //  this.items.sort();
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  
 
   get(pos) {
-    if (pos > this.items.length - 1) {
+    if (pos > this.items.length -1) {
       throw new Error('OutOfBounds');
     } else {
       return this.items[pos];
